045a67e80a1e6f5ee665dbbe230e59118e9e7d54
Steven <sturner@forge-applications.io>
2026-06-10 17:51:06 -0600

    setup cloudflare

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import cloudflare from '@astrojs/cloudflare';
 
-// https://astro.build/config
 export default defineConfig({
-	site: 'https://sturner-forge-dev.github.io',
+	site: 'https://steventurner.dev',
 	base: '/portfolio',
+	outDir: './dist/portfolio',
+	adapter: cloudflare(),
 });
+
