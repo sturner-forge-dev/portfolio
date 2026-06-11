4e3032bf13db8b38ee38638922ca04afd81a845d
Steven <sturner@forge-applications.io>
2026-06-11 14:42:29 -0600

    remove wrangler config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,8 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import cloudflare from '@astrojs/cloudflare';
 
 export default defineConfig({
 	site: 'https://steventurner.dev',
 	base: '/',
 	outDir: './dist',
-	adapter: cloudflare(),
 });
